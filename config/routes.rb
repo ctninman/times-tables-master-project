8c03da2ee9a1411ad92f5e7bf341118017392026
ctninman <89664157+ctninman@users.noreply.github.com>
2022-03-23 12:22:48 -0400

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get '/hello', to: 'application#hello'
-  # post '/hello_world', to: 'application#hello_world'
+
+  get '*path',
+  to: 'fallback#index',
+  constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
