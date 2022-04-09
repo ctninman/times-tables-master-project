7ceebfd1caec3fdb7fdb24afc65a6c3de68e5ece
ctninman <89664157+ctninman@users.noreply.github.com>
2022-04-09 01:17:10 -0400

    minor css added dashboard

--- client/src/components/FactCard.js
+++ client/src/components/FactCard.js
@@ -1,6 +1,6 @@
 function FactCard ({fact}) {
   return (
-    <div style={{backgroundColor: 'white', width: '118px', margin: '5px', padding: '5px', marginTop: '5px', borderRadius: '45%'}}>
+    <div className='dashboard-fact' style={{backgroundColor: 'white', width: '135px', height: '35px', paddingBottom: '2px',paddingTop:'8px',margin: '5px', marginTop: '5px', borderRadius: '45%'}}>
       <h3 style={{margin: '3px', textAlign: 'center'}}>{fact.multiplication_fact} = {fact.answer}</h3>
     </div>
   )
